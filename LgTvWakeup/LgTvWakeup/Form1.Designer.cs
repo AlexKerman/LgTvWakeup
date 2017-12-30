564a202c641d96c933cd968892bd37f6fae1c188
Alex Kerman <kerman@mail.ru>
2017-12-30 20:38:26 +0300

    init

---
+++ LgTvWakeup/LgTvWakeup/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace LgTvWakeup
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.openSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnNotifyIconClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openSettingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(148, 48);
+			// 
+			// openSettingsToolStripMenuItem
+			// 
+			this.openSettingsToolStripMenuItem.Name = "openSettingsToolStripMenuItem";
+			this.openSettingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.openSettingsToolStripMenuItem.Text = "Open settings";
+			this.openSettingsToolStripMenuItem.Click += new System.EventHandler(this.OnOpenSettings);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnExit);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "SerialPort:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(15, 42);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(329, 200);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
+			this.Text = "LG TV Wakeup settings";
+			this.Resize += new System.EventHandler(this.OnResize);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem openSettingsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
+
---
+++ LgTvWakeup/LgTvWakeup/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Globalization;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.ApplicationServices;
+
+namespace LgTvWakeup
+{
+	public partial class Form1 : Form
+	{
+
+		private SerialPort port;
+
+		public Form1()
+		{
+			InitializeComponent();
+			PowerManager.IsMonitorOnChanged += MonitorOnChanged;
+
+			notifyIcon1.Icon = Icon = Properties.Resources.MonPower;
+			foreach (var portName in SerialPort.GetPortNames())
+			{
+				comboBox1.Items.Add(portName);
+			}
+		}
+
+		private void MonitorOnChanged(object sender, EventArgs e)
+		{
+			bool monOn = PowerManager.IsMonitorOn;
+			using (var port = new SerialPort(comboBox1.Text, 9600, Parity.None, 8, StopBits.One))
+			{
+				port.Handshake = Handshake.XOnXOff;
+				port.Open();
+				port.WriteLine("ka 01 " + (monOn ? "01" : "00"));
+				port.Close();
+			}
+		}
+
+		//Another way, but always returned lParam = 2, so no way to find out is display turning on or off
+
+		//const int WM_SYSCOMMAND = 0x0112;
+		//const int SC_MONITORPOWER = 0xF170;
+		//const int SC_SCREENSAVE = 0xF170;
+
+		//[PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+		//protected override void WndProc(ref Message m)
+		//{
+		//	base.WndProc(ref m);
+
+		//	if (m.Msg == WM_SYSCOMMAND)
+		//	{
+		//		var cmd = m.WParam.ToInt32() & 0xFFF0;
+		//		if (cmd == SC_MONITORPOWER || cmd == SC_SCREENSAVE)
+		//		{
+		//			int state = m.LParam.ToInt32();
+		//		}
+		//	}
+		//}
+
+		private void OnResize(object sender, EventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized) Hide();
+		}
+
+		private void OnNotifyIconClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+				ShowAndBringToFront();
+		}
+
+		private void OnOpenSettings(object sender, EventArgs e)
+		{
+			ShowAndBringToFront();
+		}
+
+		private void ShowAndBringToFront()
+		{
+			Show();
+			if (WindowState == FormWindowState.Minimized)
+				WindowState = FormWindowState.Normal;
+			BringToFront();
+		}
+
+		private void OnExit(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
